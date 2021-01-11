392006d45a00e503bbbca747a1a68945f7d36593
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-11 16:35:26 +0300

    functions to go

--- functions.js
+++ functions.js
@@ -7,8 +7,9 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`)
 }
+greet("Murray")
 
 /**
  * isOdd(n):
@@ -20,8 +21,12 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  const odd = true
+if(n % 2 === 0) return !odd
+else return odd
 }
+isOdd(7)
+isOdd(10)
 
 /**
  * oddsSmallerThan(n):
@@ -33,8 +38,10 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  if (n % 2 === 0) {return n/2}
+else {return ((n-1)/2)}
 }
+oddsSmallerThan(12)
 
 /**
  * squareOrDouble(n):
@@ -47,7 +54,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
-}
+  if(n % 2) return n*n
+  else return n * 2
+  }
+  
+  squareOrDouble(4)
+  squareOrDouble(7)
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
